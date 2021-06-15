2a3976423f83a9482cad155f4db6aa16ebafdd29
Venodez <30352734+Venodez@users.noreply.github.com>
2021-06-15 17:52:39 +0200

    Retour en arrière

--- project/scripts/99999 Online.rb
+++ project/scripts/99999 Online.rb
@@ -87,49 +87,43 @@ module Online
     end
 
     def self.handle_data(data)
-        Thread.new do
-            LOCK.synchronize do
-                Thread.main.wakeup
-                case data[:type]
-                when "update_position"
-                    data[:value].each do |player| 
-                        if !@players.has_key?(player.uuid)
-                            @players[player.uuid] = GamePlayer_Event.new(player.map_id, player.x, player.y, "cynthia_hgss")
-                            $game_temp.player_new_x = $game_player.x
-                            $game_temp.player_new_y = $game_player.y
-                            $game_temp.player_transferring = true   
+        case data[:type]
+        when "update_position"
+            data[:value].each do |player| 
+                if !@players.has_key?(player.uuid)
+                    @players[player.uuid] = GamePlayer_Event.new(player.map_id, player.x, player.y, "cynthia_hgss")
+                    $game_temp.player_new_x = $game_player.x
+                    $game_temp.player_new_y = $game_player.y
+                    $game_temp.player_transferring = true   
+                else
+                    player_client = @players[player.uuid]
+                    case player.direction
+                    when 2
+                        player_client.turn_down()
+                    when 4
+                        player_client.turn_left()
+                    when 6
+                        player_client.turn_right()
+                    when 8
+                        player_client.turn_up()
+                    end
+                    if player.x != player_client.x
+                        if player.x > player_client.x
+                            player_client.move_right()
                         else
-                            player_client = @players[player.uuid]
-                            case player.direction
-                            when 2
-                                player_client.turn_down()
-                            when 4
-                                player_client.turn_left()
-                            when 6
-                                player_client.turn_right()
-                            when 8
-                                player_client.turn_up()
-                            end
-                            if player.x != player_client.x
-                                if player.x > player_client.x
-                                    player_client.move_right()
-                                else
-                                    player_client.move_left()
-                                end
-                            elsif player.y != player_client.y
-                                if player.y > player_client.y
-                                    player_client.move_down()
-                                else
-                                    player_client.move_up()
-                                end
-                            end
+                            player_client.move_left()
+                        end
+                    elsif player.y != player_client.y
+                        if player.y > player_client.y
+                            player_client.move_down()
+                        else
+                            player_client.move_up()
                         end
                     end
-                else
-                    log_info("Error: Unknown Data => " + data.to_s)
                 end
-                Thread.main.wakeup
             end
+        else
+            log_info("Error: Unknown Data => " + data.to_s)
         end
     end
 end
