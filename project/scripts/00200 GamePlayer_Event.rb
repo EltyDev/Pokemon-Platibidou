10a3def83e73f20cf8717a4495fc64a2cc1fc58b
Venodez <30352734+Venodez@users.noreply.github.com>
2021-06-15 17:27:55 +0200

    Changement de coordonné

--- project/scripts/00200 GamePlayer_Event.rb
+++ project/scripts/00200 GamePlayer_Event.rb
@@ -1,7 +1,7 @@
 class GamePlayer_Event < Game_Event
 
     def initialize(map_id, x, y, graphic_name)
-        event = RPG::Event.new(x + ::Yuki::MapLinker::OffsetX, y + ::Yuki::MapLinker::OffsetY)
+        event = RPG::Event.new(x , y)
         event.pages[0].graphic.character_name = graphic_name
         event_id = 1
         until $game_map.events[event_id] == nil
